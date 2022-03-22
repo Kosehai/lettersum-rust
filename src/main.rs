f85e5e13cf746da38c5f6884d811ec2caebcb45a
Martin <martin.wigum@gmail.com>
2022-03-22 15:16:48 +0100

    init

---
+++ src/main.rs
@@ -0,0 +1,112 @@
+use std::io::BufReader;
+use std::io::prelude::*;
+use std::path::Path;
+use std::fs::File;
+use std::collections::HashMap;
+
+fn main() {
+    assert_eq!(lettersum("".to_string()), 0);
+    assert_eq!(lettersum("a".to_string()), 1);
+    assert_eq!(lettersum("z".to_string()), 26);
+    assert_eq!(lettersum("cab".to_string()), 6);
+    assert_eq!(lettersum("excellent".to_string()), 100);
+    assert_eq!(lettersum("microspectrophotometries".to_string()), 317);
+    //load enable1 wordlist
+    let path = Path::new("enable1.txt");
+    let display = path.display();
+    
+    //Load file into memory
+    let file = match File::open(&path) {
+        Err(why) => panic!("Could not open {}: {}", display, why),
+        Ok(file) => file,
+    };
+    //Read file to String vector
+    let buf = BufReader::new(file);
+    let mut wordlist:Vec<String> = Vec::new();
+    for line in buf.lines() {
+        wordlist.push(line.unwrap());
+    }
+    println!("Loaded enable1 wordlist with: {} words", wordlist.len());
+
+    let odds = oddsums(&wordlist);
+    println!("There is {} numbers with odd sums", odds);
+    let common = mostcommonsum(&wordlist);
+    println!("The biggest common sum is {} and it contains {} words", common.0, common.1);
+    let pairswithdiff = findpairwithdiff(&wordlist, 11);
+    println!("Found a pair with same sum and diff off {}: {}:{}", pairswithdiff.2, pairswithdiff.0, pairswithdiff.1);
+}
+
+fn lettersum(letters: String) -> u32{
+    let mut sum: u32 = 0;
+    //Calculate offset so letter a is the 0th letter of the ascii table
+    let offset: u32 = ('a' as u32) - 1;
+    for c in letters.chars(){
+        //Convert char to ascii value u32 overkill
+        let val: u32 = c as u32;
+        sum += val - offset;
+    }
+    return sum;
+}
+
+
+fn oddsums(wordlist: &Vec<String>) -> u32 {
+    //check numbers with uneven sum
+    let mut odds: u32 = 0;
+    for word in wordlist {
+        if lettersum(word.to_string()) % 2 != 0{
+            odds += 1;
+        }
+    }
+
+    return odds;
+}
+
+fn mostcommonsum(wordlist: &Vec<String>) -> (u32, u32) {
+    //Create hashmap to keep track of sums
+    let mut allsums: HashMap<u32, u32> = HashMap::new();
+    //Populate the hashmap
+    for word in wordlist {
+        let wordsum = lettersum(word.to_string());
+        *allsums.entry(wordsum).or_insert(0) += 1;
+    }
+    //Rust magic to pop you the biggest key
+    let biggestkey = allsums.iter().max_by_key(|key | key.1).unwrap();
+    return (*biggestkey.0, *biggestkey.1);
+}
+
+fn findpairwithdiff(wordlist: &Vec<String>, diff: usize) -> (String, String, usize) {
+    //Generate simelar hashmap but store words in a vector (GIGA MEMORY USAGE)
+    let mut pairs: HashMap<u32, Vec<String>> = HashMap::new();
+    for word in wordlist {
+        let wordsum = lettersum(word.to_string());
+        if pairs.contains_key(&wordsum){
+            //bad error handeling D:
+            pairs.get_mut(&wordsum).unwrap().push(word.to_string())
+        } else {
+            pairs.insert(wordsum, vec!(word.to_string()));
+        }
+    }
+
+    //Nested loop probally a cleaner way to do it
+    //We save the smallest len we find and biggest then compare
+    for pair in pairs {
+        let mut smallest = String::from("");
+        let mut biggest = String::from("");
+        for s in pair.1{
+            //Need to init smallest with a real value
+            if smallest.len() == 0{
+                smallest = s.to_string();
+            }
+            if smallest.len() > s.len(){
+                smallest = s.to_string();
+            }
+            if biggest.len() < s.len() {
+                biggest = s.to_string();
+            }
+        }
+        if biggest.len() - smallest.len() >= diff && biggest != "biodegradabilities" && smallest != "zyzzyva" {
+            return (biggest.to_string(), smallest.to_string(), biggest.len() - smallest.len());
+        }
+    }
+    return ("".to_string(),"".to_string(), 0);
+}
